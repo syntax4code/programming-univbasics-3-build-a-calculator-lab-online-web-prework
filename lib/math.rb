7b0090b8bf059a11b42578aaf84126d65b428ce8
Warren Loy Harper II <warrenharper2@att.net>
2019-07-18 23:11:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,18 +3,22 @@ num1 + num2
 end
 
 def subtraction(num1, num2)
+  num1 - num2
 
 end
 
 def division(num1, num2)
+  num1 / num2
 
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 
 end
 
 def modulo(num1, num2)
+  num1 / num2 
 
 end
 
